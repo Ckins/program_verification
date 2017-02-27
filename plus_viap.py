97e24c6720c5d32f798b140be1e9acfaec469e3e
ckins <492159680@qq.com>
2017-02-27 19:00:55 +0800

    format change

--- plus_viap.py
+++ plus_viap.py
@@ -2,7 +2,7 @@
 
 import re
 import random
-#add by Pritom Rajkhowa
+# add by Pritom Rajkhowa
 import wolframalpha
 import sys
 import plyj.parser
@@ -23,52 +23,54 @@ ParserElement.enablePackrat()
 
 config = ConfigParser.RawConfigParser()
 config.read('config.properties')
-timeout=config.get('ConfigurationSection', 'timeout');
-app_id=config.get('ConfigurationSection', 'app_id');
+timeout = config.get('ConfigurationSection', 'timeout')
+app_id = config.get('ConfigurationSection', 'app_id')
+
 
 def is_number(s):
     try:
-        float(s) # for int, long and float
+        float(s)  # for int, long and float
     except ValueError:
         try:
-            complex(s) # for complex
+            complex(s)  # for complex
         except ValueError:
             return False
     return True
 
-#Time Out
-if timeout.strip()!='' and timeout.strip()!='None' and is_number(timeout.strip())!=False:
-	TIMEOUT=timeout.strip()
+# Time Out
+if timeout.strip() != '' and timeout.strip() != 'None' and is_number(timeout.strip()) != False:
+    TIMEOUT = timeout.strip()
 else:
-	TIMEOUT=60000
+    TIMEOUT = 60000
 
-if app_id.strip()!='' and app_id.strip()!='None':
-	Mathematica_id=app_id.strip()
+if app_id.strip() != '' and app_id.strip() != 'None':
+    Mathematica_id = app_id.strip()
 else:
-	Mathematica_id=None
+    Mathematica_id = None
 
-_base = ['=','==','!=','<','<=','>','>=','*','**','!','+','-','/', '%', 'ite', 'and', 'or', 'not',\
+_base = ['=', '==', '!=', '<', '<=', '>', '>=', '*', '**', '!', '+', '-', '/', '%', 'ite', 'and', 'or', 'not',\
          'implies', 'all', 'some', 'null']
-_infix_op = ['=','==','!=','<','<=','>','>=','*','**','+','-','/', '%', 'implies']
+_infix_op = ['=', '==', '!=', '<', '<=', '>', '>=', '*', '**', '+', '-', '/', '%', 'implies']
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 p = plyj.parser.Parser()
 
+
 def getParser():
-	global p
-	return p
+    global p
+    return p
+
 
 def translate(file_name):
-	if not(os.path.exists(file_name)):
-		print "File not exits"
-		return
+    if not (os.path.exists(file_name)):
+        print "File not exits"
+        return
 
-	start_time=current_milli_time()
-	p = getParser()
-	tree = p.parse_file(file_name)
-	print tree
+    start_time = current_milli_time()
+    p = getParser()
+    tree = p.parse_file(file_name)
+    print tree
 
 
-file_name='benchmark/fact.java'
-axiom=translate(file_name)
-
+file_name = 'benchmark/fact.java'
+axiom = translate(file_name)
